0cfcf2bf6166be891f95db89170f5110fc1620dd
borismarinho <48392819+borismarinho@users.noreply.github.com>
2019-04-05 14:03:52 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@ typedef struct node {
     int id;
     struct node *next;
     struct node *previous;
+	struct t_node_list *adjacency_list;
 } t_node;
 
 typedef struct {
@@ -47,15 +48,33 @@ void add_node_to_list(t_node_list* node_list, t_node *node){
     }
 }
 
+t_node_list *create_adjacency_list (t_node_list *node_list, t_node *node, t_edge edge){
+	t_node_list *adjacency_list;
+	t_node *aux_node, *src_node, *tgt_node;
+	aux_node = node_list->first;
+	while (aux_node->id != edge.source && aux_node->next != NULL){
+		aux_node = aux_node->next;
+	}
+	src_node = aux_node;
+	aux_node = node_list->first;
+	while (aux_node->id != edge.target && aux_node->next != NULL){
+		aux_node = aux_node->next;
+	}
+}
+
+
+
 void parser (FILE *stream){
     t_node_list *node_list;
     t_node *node;
+	t_edge edge;
+	char buffer[100];
     int i = 0, j, k;
+	int sz, id, source, target;
     char s[100];
     char stack[1000][100];
     char aux_stack[1000][100];
     node_list = create_node_list();
-    node = create_node(0);
     while(fgets(s, 100, stream) != NULL){
         j = 0;
         if (strstr(s, "]") != NULL){
@@ -71,15 +90,34 @@ void parser (FILE *stream){
             i--;
             strcpy(aux_stack[j], stack[i]);
             if (strstr(aux_stack[j], "node") != NULL){
-                printf("SANDUBA %c", *(strstr(aux_stack[j-1], "id") + 3));
+                strcpy(buffer, (strstr(aux_stack[j-1], "id") + 3));
+				sz = strlen(buffer);
+				buffer[sz - 1] = '\0';
+				id = atoi(buffer);
+				node = create_node(id);
+				add_node_to_list(node_list, node);
+				node->adjacency_list = create_node_list();
             }
+			if (strstr(aux_stack[j], "edge") != NULL){
+				strcpy(buffer, (strstr(aux_stack[j-1], "source") + 7));
+				sz = strlen(buffer);
+				buffer[sz - 1] = '\0';
+				source = atoi(buffer);
+				strcpy(buffer, (strstr(aux_stack[j-2], "target") + 7));
+				sz = strlen(buffer);
+				buffer[sz - 1] = '\0';
+				target = atoi(buffer);
+				printf("source = %d, target = %d,\n", source, target);
+				edge.source = source;
+				edge.target = target;
+			}
             for (k = 0; k <= j; k++){
-                printf("\nAUX\n%s\n", aux_stack[k]);
+                //printf("\nAUX\n%s\n", aux_stack[k]);
             }
         }
         else{
             strcpy(stack[i], s);
-            printf("stack[%d] = %s", i, stack[i]);
+            //printf("stack[%d] = %s", i, stack[i]);
             i++;
         }
     }
